666598e0f7d45dcf8dfeef9ccfc4cb75d93c4631
Alexander K <xpyro@ya.ru>
2012-04-30 14:47:32 +0600

    really first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# My experimental repo
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelpTranslate::Application.config.secret_token = 'd6d38735f12af31af7c7af87dd59a35319f38bdb92a846b6254afed2077dac089faade058e5e233a157570aa8960d9abdef975b898afc64e1948ee2892887503'
