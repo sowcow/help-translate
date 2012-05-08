ddb3bf646e366344c80d6e68dafb82e81b962ec9
Alexander K <xpyro@ya.ru>
2012-05-08 09:31:16 +0600

    deployment config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,21 +2,17 @@ set :application, 'translation-assistant'
 set :repository,  'git://github.com/sowcow/help-translate.git'
 
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, 'localhost'          #"your web-server here"                          # Your HTTP server, Apache/etc
-role :app, 'localhost'          #"your app-server here"                          # This may be the same as your `Web` server
-role :db,  'localhost', primary: true          #"your primary db-server here", :primary => true # This is where Rails migrations will run
-#role :db,  'localhost'          #"your slave db-server here"
+role :web, 'localhost'
+role :app, 'localhost'
+role :db,  'localhost', primary: true
 
-#set :port, 2222
+set :port, 22
 set :user, "translation-assistant"
-#set :user, 'root'
 
 set :deploy_to, "/home/translation-assistant/apps/#{application}"
-set :use_sudo, true #false
+set :use_sudo, false
 set :keep_releases, 5
-set :deploy_via, :copy
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
