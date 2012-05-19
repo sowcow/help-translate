4a819e77b109eeff1afa68298635a1c1b395318d
Alexander K <xpyro@ya.ru>
2012-05-19 04:40:32 +0000

    no duplicate translations per word + some css

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -2,4 +2,5 @@ class Translation < ActiveRecord::Base
   attr_accessible :content, :word_id #, :translator_id, :word_id
   belongs_to :word
   belongs_to :translator, polymorphic: true  
+  validates_uniqueness_of :content, :scope => :word_id
 end
