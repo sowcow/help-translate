1e898f7d9caf1149956576a28e578fd9b9948c40
Alexander K <xpyro@ya.ru>
2012-05-19 03:52:51 +0000

    ...

--- app/models/word.rb
+++ app/models/word.rb
@@ -6,17 +6,6 @@ class Word < ActiveRecord::Base
   validate :not_a_number
   scope :has_translations, :conditions => "words.id IN (SELECT word_id FROM translations)"
 
-  #where(:submitted => true)
-  #named_scope :having_translations, {
-  #  select:     'words.*',
-  #  joins:      'inner join translation on words.id = translations.word_id',
-  #  conditions: ''}
-  #  select: "episodes.*",
-  #  joins: "INNER JOIN series ON series.id = clips.owner_id INNER JOIN shows on shows.id = series.show_id", 
-  #  conditions: "shows.visible = 1 AND clips.owner_type = 'Series' "
-  #}
-
-
   def similars count=3
     Word.has_translations.select("*, content <-> text('#{content}') AS dist").where("id <> #{id}").order('dist').limit(count)
     #Word.select("DISTINCT content, content <-> text('#{content}') AS dist").where("id <> #{id}").order('dist').limit(count)
